13d984bd3e64bb561b695a342ec1b9c0d8713c24
mitiak <mitiak@gmail.com>
2018-07-28 13:17:39 +0300

    babynames.py completed

--- babynames/babynames.py
+++ babynames/babynames.py
@@ -40,8 +40,48 @@ def extract_names(filename):
   followed by the name-rank strings in alphabetical order.
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
-  # +++your code here+++
-  return
+  f=open(filename, 'rU')
+  file_text = f.read()
+  res = []
+  dict_names = {}
+
+  # Parse the year
+  year_line = re.findall(r'<h3 align="center">Popularity in (\d\d\d\d)</h3>', file_text)
+  year_str = year_line[0]
+  res.append(year_str)
+
+  # Parse the names
+  name_lines = re.findall(r'<tr align="right"><td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', file_text)
+  for name_line in name_lines:
+    boy_name = name_line[1]
+    girl_name = name_line[2]
+    rating = int(name_line[0])
+
+    if boy_name in dict_names:
+      if rating < dict_names[boy_name]:
+        dict_names[boy_name] = rating  
+    else:
+      dict_names[boy_name] = rating
+
+    if girl_name in dict_names:
+      if rating < dict_names[girl_name]:
+        dict_names[girl_name] += rating
+    else:
+      dict_names[girl_name] = rating
+
+  for i in dict_names.items():
+    res.append(i[0] + ' ' +str(i[1]))
+    #print i[0] + '->' + str(i[1])
+
+    #print name_line[1] + '->' + name_line[0] + ', ' + name_line[2] + '->' + name_line[0]
+  
+  res = sorted(res[:])
+  '''
+  for i in res:
+    print i
+  '''
+  f.close()
+  return res
 
 
 def main():
@@ -60,7 +100,20 @@ def main():
     summary = True
     del args[0]
 
-  # +++your code here+++
+    for filename in args:
+      names_list = extract_names(filename)
+      f = open(filename+'.summary', 'w')
+      for name_line in names_list:
+        f.write(name_line + '\n')
+      f.close()
+
+  else:
+    names_list = extract_names(args[0])
+    for name_line in names_list:
+      print(name_line)
+
+
+  
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
   
